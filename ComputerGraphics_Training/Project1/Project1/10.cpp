5009a12b43aecb295f03fc41930397157fc56915
ysj4415@home <68343822+ysj4415@users.noreply.github.com>
2022-10-11 10:21:11 +0900

    10번

--- ComputerGraphics_Training/Project1/Project1/10.cpp
+++ ComputerGraphics_Training/Project1/Project1/10.cpp
@@ -72,11 +72,11 @@ GLfloat rect[15] = { -0.8, -0.2, 0,
 					-0.2, -0.8, 0,
 					-0.8, -0.8, 0,
 					-0.5, -0.2, 0};
-GLfloat rect_finalshape[15] = { -0.8, -0.5, 0,
-							-0.2, -0.5, 0,
-							-0.35, -0.85, 0,
-							-0.65, -0.85, 0,
-							-0.5, -0.2, 0 };
+GLfloat rect_finalshape[15] = { -0.75, -0.5, 0,
+							-0.25, -0.5, 0,
+							-0.33, -0.85, 0,
+							-0.67, -0.85, 0,
+							-0.5, -0.15, 0 };
 int rect_ebo[9] = { 0, 1, 2,
 					0, 2, 3,
 					0, 1, 4};
@@ -345,7 +345,7 @@ GLvoid Reshape(int w, int h)
 
 void make_vertexShader()
 {
-	vertexsource = filetobuf("vertex.glsl");
+	vertexsource = filetobuf("vertex_2d.glsl");
 
 	//--- 버텍스 세이더 객체 만들기
 	vertexshader = glCreateShader(GL_VERTEX_SHADER);
@@ -508,10 +508,7 @@ void changebuffer()
 		{
 		case 0:
 			arr = ShapeAnim(S_LINE);
-			for (int i = 0; i < arrlen(line); i++)
-			{
-				cout << arr[i] << ", " << endl;
-			}
+
 			glBufferData(GL_ARRAY_BUFFER, arrlen(line) * sizeof(GLfloat), arr, GL_STATIC_DRAW);
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 			break;
